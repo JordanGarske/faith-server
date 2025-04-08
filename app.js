dd1f379bf8b7b0f0a211cf991d8ef878a27ba718
jordan garske <jordago9980@yahoo.com>
2025-04-08 13:45:52 -0500

    updated varibles and build

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT ||8080;
+const GMAIL_PASS = process.env.GMAIL_PASS ;
+
 
 app.listen(PORT, () =>console.log("Server started"))
 
@@ -14,7 +16,7 @@ app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, "./build/client/index.html"));
 });
 
-const GmailPass = process.env.GMAILPASS;
+
 const transporter = nodemailer.createTransport({
     service: "gmail", 
     host: 'stmp.gmail.com',
@@ -22,7 +24,7 @@ const transporter = nodemailer.createTransport({
     secure:false,
     auth: {
       user: "jordan.garske.j@gmail.com",
-      pass: GmailPass
+      pass: GMAIL_PASS
     }
   });
 
@@ -40,20 +42,3 @@ app.post("/api/send", (req, res) => {      const mailOptions = {
      res.status(200).send("Email sent successfully");
   });
 });    
-//       user: "jordan.garske.j@gmail.com",
-//       pass: "pywk siqp psly smvj"
-// const mailOptions = {
-//     from: 'jordan.garske.j@gmail.com',
-//     to: 'jordan.garske.j@gmail.com',
-//     subject: 'Hello world?',
-//     text: 'Hello world?',
-//     html: '<b>Hello world?</b>'
-//   };      
-// const sendMail = async(transporter, mailOptions) =>{
-//     try{
-//         await transporter.sendMail(mailOptions)
-//     }catch(error){
-//         console.log(error)
-//     }
-// }
-// sendMail(transporter, mailOptions);
